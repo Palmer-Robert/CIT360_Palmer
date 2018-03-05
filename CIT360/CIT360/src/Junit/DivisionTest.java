a95a5336f31486dcd00407d3cc1e1d45a960d15a
Palmer-Robert <robert.palmer1229@gmail.com>
2018-03-05 10:51:30 -0700

    Adding Junit stuff

---
+++ CIT360/CIT360/src/Junit/DivisionTest.java
@@ -0,0 +1,14 @@
+package Junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DivisionTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
---
+++ CIT360/CIT360/src/Junit/MyJunit.java
@@ -0,0 +1,15 @@
+package Junit;
+
+public class MyJunit {
+	
+	public int division(int num1, int num2) {
+		
+		return num1 / num2;
+	}
+	
+	public int addition(int add1, int add2) {
+		
+		return add1 + add2;
+	}
+
+}
